7532a8e550c742c1061d6cef6faa6aee094f1032
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-27 11:15:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+//your JS code here. If required. 
+ const line=document.getElementById("line");
+
+setInterval(()=>{
+	const current=( window.getComputedStyle(line).transform);
+	line.style.transform="rotate(2deg) "+current;
+},20)
